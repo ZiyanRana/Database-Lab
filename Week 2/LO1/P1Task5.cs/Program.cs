96904559ec37f1610d183a0e1f5ef6ca169b32e8
ZiyanRana <mziyanrana@gmail.com>
2026-03-02 00:19:48 +0500

    Added task1

---
+++ Week 2/LO1/P1Task5.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string text = Console.ReadLine();
+
+        foreach (char ch in text)
+        {
+            Console.WriteLine(ch);
+        }
+    }
+}
