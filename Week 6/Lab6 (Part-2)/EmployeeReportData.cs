a45e3f2cb3e67f96eb25116eb22491b5318cf06a
ZiyanRana <mziyanrana@gmail.com>
2026-03-18 23:23:15 +0500

    Lab 6 Part-2 Completed

--- Week 6/Lab6 (Part-2)/EmployeeReportData.cs
+++ Week 6/Lab6 (Part-2)/EmployeeReportData.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Lab6
 {
-    internal class EmployeeReportData
+    public class EmployeeReportData
     {
+        public string EmployeeName { get; set; }
+        public string Title { get; set; }
+        public DateTime HireDate { get; set; }
     }
 }
---
+++ Week 6/Lab6 (Part-2)/OrderData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class OrderData
+    {
+        public int OrderID { get; set; }
+        public string Region { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
